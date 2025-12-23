e008f0b37225b7e8eac2d17555ef8eeadd0febe0
包子叔 <1771575735@qq.com>
2025-12-23 23:57:16 +0800

    Update main.go

--- cmd/hugo-client/main.go
+++ cmd/hugo-client/main.go
@@ -10,24 +10,19 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 
-	"hugo-visual-client/internal/app"
+	internalapp "hugo-visual-client/internal/app"
 )
 
 func main() {
 	// Create Fyne application
 	myApp := app.New()
-	myApp.SetMetadata(&app.Metadata{
-		Name:        "Hugo Visual Client",
-		Description: "A visual client for Hugo static site generator",
-		Version:     "1.0.0",
-	})
 
 	// Create main window
 	myWindow := myApp.NewWindow("Hugo Visual Client")
 	myWindow.Resize(fyne.NewSize(1200, 800))
 
 	// Initialize application controller
-	appController, err := app.NewAppController(myWindow)
+	appController, err := internalapp.NewAppController(myWindow)
 	if err != nil {
 		log.Fatal("Failed to initialize application:", err)
 	}
